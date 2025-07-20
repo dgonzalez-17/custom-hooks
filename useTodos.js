441773061888bb8c0f9143be8b650c10ca6f8bcd
Yesid Gonzalez <workyessid@gmail.com>
2025-07-20 13:59:38 -0500

    first commit

---
+++ useTodos.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos || []));
+  }, [todos]);
+
+  const onNewTodo = (newTodo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: newTodo,
+    };
+
+    dispatch(action);
+  };
+
+  const onRemoveTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const onToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter((todo) => !todo.done).length;
+
+  return {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    onNewTodo,
+    onRemoveTodo,
+    onToggleTodo,
+  };
+};
